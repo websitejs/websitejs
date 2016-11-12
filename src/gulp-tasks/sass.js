60c4d08c1381827acf97b4f2521d8098c6cad30e
Rocco Janse <roccojanse@outlook.com>
2016-11-12 01:54:07 +0100

    Updated .gitignore Updated sass gulp task

--- src/gulp-tasks/sass.js
+++ src/gulp-tasks/sass.js
@@ -7,6 +7,7 @@ module.exports = function(plugins) {
     
     gulp.add('sass', function(done) {
 
+        // paths
         var src = config.paths.src + config.paths.scss.src,
             dest = config.paths.dest + config.paths.scss.dest,
             docs = config.paths.dest + config.paths.scss.docs;
@@ -14,6 +15,7 @@ module.exports = function(plugins) {
         // cleanup
         plugins.del.sync([dest + '*.css']);
 
+        // build
         gulp.src(src + '/**/*.scss')
             .pipe(plugins.sourcemaps.init())
             .pipe(plugins.rename({ suffix: '.min' }))
@@ -40,35 +42,5 @@ module.exports = function(plugins) {
             .resume();
         
         done();
-
-
-
-        // // cleanup first
-        // plugins.del.sync([config.paths.css.dest + '*.css']);
-
-        // var sassFiles = gulp.src(config.paths.css.src + '/*.scss')
-        //     //.pipe(plugins.sourcemaps.init())
-        //     .pipe(plugins.sass({
-        //         outputStyle: 'expanded',
-        //         defaultEncoding: 'utf-8',
-        //         unixNewlines: false,
-        //         stopOnError: true,
-        //         cacheLocation: config.srcRoot + '.sass-cache/',
-        //         precision: 4,
-        //         sourceMap: true,
-        //         sourceMapEmbed: true,
-        //         outFile: '.'
-        //     })
-        //     .on('error', plugins.sass.logError))
-        //     .pipe(plugins.autoprefixer('last 3 version'))
-        //     .pipe(gulp.dest(config.paths.css.dest));
-        
-        // return plugins.es.pipe(sassFiles)
-        //     .pipe(plugins.sourcemaps.init({ loadMaps: true, includeContent: false, sourceRoot: config.paths.css.src }))
-        //     .pipe(plugins.rename({ suffix: '.min' }))
-        //     .pipe(plugins.cssnano({ zindex: false }))
-        //     .pipe(plugins.sourcemaps.write('.'))
-        //     .pipe(gulp.dest(config.paths.css.dest))
-        //     .pipe(plugins.connect.reload());
     });
 };
