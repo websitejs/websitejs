0a3fb7aae8410548af2b577f6479e5b6393dc48a
Rocco Janse <rocco.janse@valtech.nl>
2017-02-24 14:46:55 +0100

    Updated styleguide and basic comonents

---
+++ src/components/cc02-footer/footer.js
@@ -0,0 +1,45 @@
+(function($, viewport) {
+    'use strict';
+
+    /**
+     * Footer functionality.
+     * @class Footer
+     * @param {jQueryElement} $element jQuery Element to upgrade with this class.
+     * @author Rocco Janse, rocco.janse@valtech.nl
+     
+     */
+    var Footer = function($element) {
+        /** @lends Footer# */
+
+        /**
+         * @type {jQueryElement}
+         */
+        this.$element = $element;
+
+        this.init();
+        
+        return this;
+    };
+
+    $.extend(Footer.prototype, /** @lends Footer# */ {
+
+        /**
+         * Initializes component.
+         * @public
+         */
+        init: function() {
+            console.log('Inited Footer.');
+        }
+
+    });
+
+    window['Footer'] = Footer;
+
+    // The component registers itself to the componentHandler in the global scope.
+    ComponentHandler.register({
+        constructor: Footer,
+        classAsString: 'Footer',
+        cssClass: 'js-footer'
+    });
+
+})(jQuery, window.viewport);
