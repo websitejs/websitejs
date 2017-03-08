7abb75192478361444b08c63757a47ad241cbd04
Rocco Janse <rocco.janse@valtech.nl>
2017-03-08 07:10:38 +0100

    Added base Utils object

---
+++ src/js/Utils.js
@@ -0,0 +1,26 @@
+// (function($) {
+    'use strict';
+
+    /**
+     * Utility functions not belonging to specific objects or classes
+     *
+     * @author Rocco Janse <rocco.janse@valtech.nl>
+     * @class Utils
+     */
+    var Utils = {
+
+        /**
+         * Returns file extension from path.
+         * @param {String} path Path to return extension from.
+         */
+        getFileExtension: function(path) {
+            var regex = /(?:\.([^.]+))?$/;
+            var cleanPath = path.split('?')[0];
+            return regex.exec(cleanPath)[1];
+        }
+
+
+    };
+
+    window['Utils'] = Utils;
+// });
