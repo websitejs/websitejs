038f766b8773846b4356ea5e35718b6ce242ab5b
NL\sharon.vander.geest <sharon.vander.geest@efocus.nl>
2017-03-17 17:51:11 +0100

    DAFNEW component - header, build html, css, js - header old style DAF

--- src/components/cc01-header/header.js
+++ src/components/cc01-header/header.js
@@ -6,7 +6,6 @@
      * @class Header
      * @param {jQueryElement} $element jQuery Element to upgrade with this class.
      * @author Rocco Janse, rocco.janse@valtech.nl
-     
      */
     var Header = function($element) {
         /** @lends Header# */
@@ -17,7 +16,7 @@
         this.$element = $element;
 
         // this.init();
-        
+
         return this;
     };
 
@@ -29,7 +28,59 @@
          */
         init: function() {
             console.log('Inited Header.', this);
+            this.calculateButtonWidth();
+            // this.calculateWhichButtons();
+        }, 
+
+        calculateButtonWidth: function() {
+            // loop through all li's in certain class, 
+            // check if width is more then max container width of page - take predefined widths of the styleguide
+            // if it's less, show first part js-all-buttons
+            // if it's more, show second part js-more-button
+            // check http://stackoverflow.com/questions/3808808/how-to-get-element-by-class-in-javascript
+
+            // var buttonArray = document.getElementById('js-all-buttons').getElementsByTagName('li');
+            // var buttonWidth = 0;
+            // // console.log(buttonLiArray);
+            // for(var i = 0; i < buttonLiArray.length; i++) {
+            //     buttonWidth += parseInt(buttonLiArray[i].clientWidth);
+            //     // console.log(buttonWidth);
+            //     if(i === (buttonLiArray.length - 1)) {
+            //         // console.log(buttonWidth);
+            //         // return buttonWidth;
+            //         console.log(viewport);
+            //         if(buttonWidth > viewport.is('sm')) {  
+            //             console.log('shoot');
+            //             document.getElementById('js-more-button').style.display = "table-cell";
+            //             document.getElementById('js-all-buttons').style.display = "none";
+            //         } else {
+            //             document.getElementById('js-all-buttons').style.opacity = "1";
+            //         }
+            //     }
+            // }
+
+            //jQuery
+            var buttonWidth = 0;
+            $(".header-button").each(function(index, value) {
+                console.log($(".header-button"));
+                console.log($(this).width());
+                buttonWidth += parseInt($(this).width());
+                console.log(buttonWidth,' breeeeeed');
+                // if the width of all buttons are calculated
+                // check first if the viewport is more then mobile (more then sm)
+                // if so, check if buttonWidth is more then the current total width of ul.
+                if(buttonWidth > $('.navbar__standard-menu').width() || viewport.is('<sm')) {
+                    console.log('successss');
+                    $('.navbar__standard-menu').css('display', 'none');
+                    $('.navbar__more-menu').css('display', 'block');
+                } else {
+                    $('.navbar__standard-menu').css('opacity', '1');
+                }
+            });
+
         }
+
+        // calculateWhichButtons: function() {}
 
     });
 
@@ -42,4 +93,4 @@
         cssClass: 'js-header'
     });
 
-})(jQuery, window.viewport);
+})(jQuery, ResponsiveBootstrapToolkit);
