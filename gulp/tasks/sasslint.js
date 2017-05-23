ad5cde831a40ea8136ddcc9478c434ba0c0fbf94
Rocco Janse <rocco.janse@valtech.nl>
2017-05-23 16:46:55 +0200

    Updated Sass Lint Fixed linting issues

--- gulp/tasks/sasslint.js
+++ gulp/tasks/sasslint.js
@@ -11,19 +11,17 @@ module.exports = function() {
     var srcGlob = [
             config.srcPath + '/styles.scss',
             config.srcPath + '/scss/**/*.scss'
-        ],
-        dest = config.destPath + '/css',
-        docs = config.destPath + '/docs/styles';
+        ];
 
     gulp.task('sasslint', function () {
     return gulp.src(srcGlob)
 
         .pipe(cached('sasslint'))
         .pipe(sasslint({
-            configFile: config.srcPath + '/scss/.config/_sass-lint.yml'
+            configFile: './.sass-lint.yml'
         }))
         .pipe(sasslint.format())
         .pipe(sasslint.failOnError())
-});
+    });
 
 };
